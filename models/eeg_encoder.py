ee2669fefb3331046063a351c7f8b975a2f058cd
MatthewS65537 <136693480+MatthewS65537@users.noreply.github.com>
2023-10-26 23:02:34 +0800

    Bug Fix + Implement ImgHead()
    + Fixed bug in forward function of EEGEncoder()
    + Implemented ImgHead() but didn't test

--- models/eeg_encoder.py
+++ models/eeg_encoder.py
@@ -18,7 +18,31 @@ class TextHead(nn.Module):
         x = self.activation(self.output_layer(x))
         return x
 
-# class ImgHead(nn.Module):
+class ImgHead(nn.Module):
+    def __init__(self, input_dim=500, output_dim=1024, hidden_dim=1024, num_layers=2):
+        super(ImageHead, self).__init__()
+        self.hidden_layers = nn.ModuleList()
+        self.hidden_layers.append(nn.Linear(input_dim, hidden_dim))
+        for i in range(num_layers - 1):
+            self.hidden_layers.append(nn.Linear(hidden_dim, hidden_dim))
+        self.output_layer = nn.Linear(hidden_dim, output_dim)
+        self.activation = nn.ReLU()
+
+    def forward(self, x, pool=True):
+        for layer in self.hidden_layers:
+            x = self.activation(layer(x))
+        x = self.activation(self.output_layer(x))
+        if not pool:
+            return x
+        sz = x.shape
+        # Pool all in single batch together
+        # Here, each batch has 128 signals corresponding to 128 channels
+        pooler = torch.zeros(sz[:-2] + sz[-1])
+        # Iterate through items in batch
+        for i in range(sz[-2]):
+            pooler += x[:][i][:]
+        pooler /= sz[-2]
+        return pooler
 
 class EEGEncoder(nn.Module):
     def __init__(self, txt_in_feat=840, img_in_feat=500, enc_feat=1024, dec_emb_sz=768, enc_nhead=8, enc_dim_ff=2048, num_enc_layers=8):
@@ -31,12 +55,12 @@ class EEGEncoder(nn.Module):
         self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_enc_layers)
         self.fc_proj = nn.Linear(enc_feat, dec_emb_sz)
 
-    def forward(self, mode, input_data_batch=None, input_masks_batch=None, input_masks_invert=None):
+    def forward(self, mode, input_data_batch=None, input_masks_batch=None, input_masks_invert=None, pool_img_head=True):
         if mode == "TXT":
             encoded_embedding = self.txt_head(input_data_batch)
         elif mode == "IMG":
-            encoded_embedding = self.img_head(input_data_batch)
+            encoded_embedding = self.img_head(input_data_batch, pool_img_head)
 
-        encoded_embedding = self.encoder(input_data_batch, src_key_padding_mask = input_masks_invert)
+        encoded_embedding = self.encoder(encoded_embedding, src_key_padding_mask = input_masks_invert)
         encoded_embedding = F.relu(self.fc_proj(encoded_embedding))
         return encoded_embedding
