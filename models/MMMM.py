18cd7bcd4714af47b4f0a440f9213bbc0ec86eac
MatthewS65537 <136693480+MatthewS65537@users.noreply.github.com>
2023-11-06 11:19:43 +0800

    Update MMMM.py

--- models/MMMM.py
+++ models/MMMM.py
@@ -9,16 +9,21 @@ from FCN import *
 from Branch import *
 
 class MMMM(nn.Module):
-    def __init__(self, eeg_encoder, device="cuda"):
-        super(MMMM, self).__init__()
-        self.eeg_encoder = eeg_encoder
-        self.branches = {}
-        self.meta_head = FCN(
+    def __init__(
+        self,
+        eeg_encoder,
+        meta_head=FCN(
             input_dim=768,
             output_dim=768,
             num_layers=4,
             device=device
-            )
+            ),
+        device="cuda"
+        ):
+        super(MMMM, self).__init__()
+        self.eeg_encoder = eeg_encoder
+        self.branches = {}
+        self.meta_head = meta_head
         self.device = device
 
     def add_branch(self, name, branch):
@@ -27,6 +32,7 @@ class MMMM(nn.Module):
     def forward(self, mode, args_dict):
         if mode == "EEG-TEXT-BART":
             encoded_embedding = self.eeg_encoder(mode, args_dict)
-            encoded_embedding = self.meta_head
+            encoded_embedding = self.meta_head(encoded_embedding)
+            args_dict["input_data_batch"] = 
             out = self.branch["EEG-TEXT-BART"](args_dict)
             return out
