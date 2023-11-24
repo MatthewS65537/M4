e02b5f0c4a136c737646d87be454470768f0e5a4
matthews65537 <matthews65537@outlook.com>
2023-11-24 10:04:46 +0000

    Added Grid Search Tune for EEG-TEXT-BART

--- pip_install.sh
+++ pip_install.sh
@@ -1,7 +1,7 @@
 pip install -q scipy
 pip install -q h5py
 pip install -q matplotlib
-pip install -q torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
+pip install -q torch torchvision torchaudio
 pip install -q transformers
 pip install -q fuzzy_match
 pip install -q nltk
